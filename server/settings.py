6f3cbc93c082e5b07fecc4975fbf8693aa90ee1f
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-07-16 20:15:40 +0700

    feat: toogle-video button

--- server/settings.py
+++ server/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "blog_thesis",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "1",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
