c237c11ddd79ac6622da8ad0e708f32429a92e78
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-07-29 00:14:37 +0700

    update doc

--- helpus/forms.py
+++ helpus/forms.py
@@ -3,7 +3,11 @@ from accounts.models import User
 
 
 
-class ContributeRegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ("fullname", "phone", "facebook")
+# class ContributeRegisterForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ("fullname", "phone", "facebook")
+    
+class ContributeRegisterForm(forms.Form):
+    word = forms.CharField()
+    file = forms.FileField()
