f7750412896d7e42e919a1ae3e204ce3a72c3f3a
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-08-02 02:47:15 +0700

    fixbug: setting

--- server/settings.py
+++ server/settings.py
@@ -99,16 +99,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT')
-    }
-}
 
 
 # Password validation
