b712e5ee69d9087794fbf78bda5ea4b1b577f3c5
tangoc2712 <taquangngoc.hh3@gmail.com>
2022-08-02 01:42:54 +0700

    fix: env

--- server/settings.py
+++ server/settings.py
@@ -166,8 +166,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Send mail SMTP
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "taquangngoc.hh31@gmail.com"
-EMAIL_HOST_PASSWORD = "tqqnnt27"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
